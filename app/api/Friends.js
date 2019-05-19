973347910a3a35122e252c1e565bb49dcc64c933
kranthijulakantiwork <kranthi.julakanti.work@gmail.com>
2019-05-19 17:11:27 +0530

    inter

--- app/api/Friends.js
+++ app/api/Friends.js
@@ -5,7 +5,6 @@ import Http from 'app/api/HTTP';
 module.exports = {
   getFriends: async currentUser => {
     const params = {
-      user_id: currentUser.server_id,
       auth_key: currentUser.auth_key
     };
     const apiParams = {
